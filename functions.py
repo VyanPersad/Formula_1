ec456b7ddcc1e82cae7ce6bd82e7a50eab05f433
VyanPersad <vyanpersad@gmail.com>
2024-08-03 20:59:48 -0400

    Python not writing to subfolders.

--- functions.py
+++ functions.py
@@ -1,6 +1,15 @@
-import seaborn as sns
-import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+import html5lib
+import os
+
+def makeFolder(folderpath):
+    if os.path.exists(folderpath):
+        return
+        #print("Path Exists")
+    else:
+        os.makedirs(folderpath)
+        print("Path does not exist creating....")
 
 def read_from_file(filepath, test=0, n=5, col_Names = [], sheet = 0):
     filetype = filepath.split('.')[1]
@@ -35,23 +44,33 @@ def read_from_file(filepath, test=0, n=5, col_Names = [], sheet = 0):
             print(dataFrame.head(n))
 
         return dataFrame
+ 
+def simple_scraper(url, n=5, test = 0, table_Num = 0, match_term = None):
+    #If the url has multiple tables then set the table_Num to get that table
+    #The dafault is set to 0 which will display the first one.
+    if (match_term == None):
+        dataFrame_list = pd.read_html(url)
+        dataFrame = dataFrame_list[table_Num]
+        if (test == 0):
+            print('Set test to 1 to view sample datraframes, Default is the first 5 rows, set n to vary the number of rows.')
+        elif (test == 1):
+            print(dataFrame.head(n))
+    else:
+        dataFrame_list = pd.read_html(url, match=f'{match_term}')
+        dataFrame = dataFrame_list[table_Num]
+        if (test == 0):
+            print('Set test to 1 to view sample datraframes, Default is the first 5 rows, set n to vary the number of rows.')
+        elif (test == 1):
+            print(dataFrame.head(n))
+    
+    return dataFrame
 
-def barPlot(dataFrame, index = None, colNames = None, stacked=False, title='None', xlabel='X-Axis', ylabel='Y-Axis', width=10, height=5):
-    #colnames is an array of the column names you wish to plot.
-    #the index param is the x axis
-    df = dataFrame.dropna()
-    df = df[colNames]
-    df.set_index(index).plot.bar(rot=0, fontsize = 11, stacked=stacked, figsize=(width,height))
-    plt.title(title)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.legend(loc = (1.04,0.8))
-    plt.tight_layout()
-    plt.show()
+def write_to_csv(dataFrame, destpath, file_name):
+    dataFrame.to_csv(f'/{destpath}/{file_name}.csv', mode = 'a', index=False)
+    return print('CSV write complete.')
 
-filepath = 'data\locations\Grand_Paris_zone.csv'
-colnames = ['Venue','Capacity']
+def write_to_xl(dataFrame, destpath, file_name, sheet_name = '1'):
+    makeFolder(f'/{destpath}/{file_name}.xlsx')
 
-df = read_from_file(filepath)
+    return dataFrame.to_excel(f'/{destpath}/{file_name}.xlsx', sheet_name=sheet_name, index=False)
 
-barPlot(df, index='Venue', colNames=colnames)
--- getData.py
+++ getData.py
@@ -1,44 +1,23 @@
-import pandas as pd
+from functions import*
+
+url1 = 'https://en.wikipedia.org/wiki/1988_Summer_Olympics_medal_table'
+url2 = 'https://en.wikipedia.org/wiki/1992_Summer_Olympics_medal_table'
+url3 = 'https://en.wikipedia.org/wiki/1996_Summer_Olympics_medal_table'
+url4 = 'https://en.wikipedia.org/wiki/2000_Summer_Olympics_medal_table'
+url5 = 'https://en.wikipedia.org/wiki/2004_Summer_Olympics_medal_table'
+url6 = 'https://en.wikipedia.org/wiki/2008_Summer_Olympics_medal_table'
+url7 = 'https://en.wikipedia.org/wiki/2012_Summer_Olympics_medal_table'
+url8 = 'https://en.wikipedia.org/wiki/2016_Summer_Olympics_medal_table'
+url9 = 'https://en.wikipedia.org/wiki/2020_Summer_Olympics_medal_table'
+
+urlArr = [url1,url2,url3,url4,url5,url6,url7,url8,url9]
+
+dest = 'data\\olympics'
+file_name = 'MedalRanks'
 
 
-'''Venues'''
+url = f'https://en.wikipedia.org/wiki/1988_Summer_Olympics_medal_table'
+df = simple_scraper(url, test=0, match_term='Rank')
+write_to_csv(df, destpath=dest,file_name=file_name)
 
-def simple_scraper(url, n=5, test = 0, table_Num = 0, match_term = None):
-    #If the url has multiple tables then set the table_Num to get that table
-    #The dafault is set to 0 which will display the first one.
-    if (match_term == None):
-        dataFrame_list = pd.read_html(url)
-        dataFrame = dataFrame_list[table_Num]
-        if (test == 0):
-            print('Set test to 1 to view sample datraframes, Default is the first 5 rows, set n to vary the number of rows.')
-        elif (test == 1):
-            print(dataFrame.head(n))
-    else:
-        dataFrame_list = pd.read_html(url, match=f'{match_term}')
-        dataFrame = dataFrame_list[table_Num]
-        if (test == 0):
-            print('Set test to 1 to view sample datraframes, Default is the first 5 rows, set n to vary the number of rows.')
-        elif (test == 1):
-            print(dataFrame.head(n))
-    
-    return dataFrame
-
-def write_to_csv(dataFrame, destpath, file_name):
-    return dataFrame.to_csv(f'{destpath}/{file_name}.csv', mode='a', index=False, header=False)
-
-'''
-venuRL = 'https://en.wikipedia.org/wiki/2024_Summer_Olympics'
-label = ['Grand Paris zone','Paris Centre zone','Versailles zone','Outlying venues','Non-competitive']
-
-for i in range(5):
-    df = simple_scraper(venuRL, test=1, table_Num=(i+4))
-    label_row = {label[i]}
-    df_row = pd.DataFrame([label_row])
-    write_to_csv(df_row, 'data', 'allTables')
-    write_to_csv(df, 'data', 'allTables')
-'''
-
-archURL = 'https://en.wikipedia.org/wiki/Archery_at_the_2024_Summer_Olympics_%E2%80%93_Women%27s_team'
-
-df = simple_scraper(archURL, n=5, test=1, table_Num=7)
-write_to_csv(df, 'data/archery', 'archWomensRank')
+#write_to_xl(df, destpath=dest, file_name=file_name, sheet_name=f'{i}')
